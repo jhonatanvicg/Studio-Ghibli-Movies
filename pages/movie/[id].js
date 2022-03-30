d861e2f5c226bd9c959360be18fc577a4657a21f
Jhonatan Victorio <jhonatanvicg@gmail.com>
2022-03-30 12:17:40 -0600

    Solucion de error al recargar la pagina desde el id de una pelicula

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,4 +1,4 @@
-import {useContext, useEffect} from "react";
+import {useContext, useEffect, useState} from "react";
 import { useRouter } from 'next/router';
 import Image from "next/image";
 import Banner from "../../components/Banner";
@@ -6,83 +6,101 @@ import Footer from "../../components/Footer";
 import BtnFavorites from "components/BtnFavorites";
 import AppContext from "context/AppContext";
 import useGetMovies from "hooks/useGetMovies"
+import axios from "axios";
 
 
 let movieSelected = ''
+let response = ''
+let auxId = ''
 
 const MovieDetail = ()=>{
-
+  const[prove,setProve] = useState([])
+  const router = useRouter()
+  
+  /* 
   if(typeof window !== "undefined"){
     console.log("Cargando componente")
     const { getMovieById } = useContext(AppContext)
-  
-    const router = useRouter()
+    
     movieSelected = getMovieById(router.query.id)
     if(movieSelected == undefined){
       movieSelected =     JSON.parse(localStorage.getItem(router.query.id))
+      auxId = router.query.id
     }
-
+    
   }
+  */
+  
+  
+    useEffect( async()=>{
+      console.log(router.query.id)
+      try{
+        response = await axios.get(`https://ghibliapi.herokuapp.com/films/${router.query.id}`);
+        setProve(response.data)
+        console.log("Este es el prove: ",prove.data)
+      }catch(error){
+      }
+     },[router.query.id])
 
 
   return(
     <>
       <div className="Container__Detail">
-        <Banner pathNow="Movie" bannerMovieSelected={movieSelected.movie_banner} />
+        <Banner pathNow="Movie" bannerMovieSelected={prove.movie_banner} />
         <div className="MovieDetail__Container">
 
           <div className="MovieDetail__Info">      
             <div className="Info__Movie-Left">
-              <img src={movieSelected.image} alt="" />
+              <img src={prove.image} alt="" />
               <div className="Movie-Left__Rate">
                 <p>Rating Score:</p>
-                <p>{movieSelected.rt_score/10}</p>
+                <p>{prove.rt_score/10}</p>
                 <img src="/images/star.png" alt="" />
               </div>
             </div>
 
             <div className="Info__Movie-Right">
               <div className="Movie-Right__Title">
-                <p>{movieSelected.title}</p>
+                <p>{prove.title}</p>
               </div>
               <div className="Movie-Right__Image">  
-                <img src={movieSelected.image} alt="" />
+                <img src={prove.image} alt="" />
                 <div className="Movie-Left__Rate">
                 <p>Rating Score:</p>
-                <p>{movieSelected.rt_score}</p>
+                <p>{prove.rt_score}</p>
                 <img src="/images/star.png" alt="" />
               </div>
               </div>
               <div className="Movie-Right__Description">
                 <p>
-                {movieSelected.description}
+                {prove.description}
                 </p>
               </div>
               <div className="Movie-Right__Duration">
                 <p>Duration: </p>
-                <p>{movieSelected.running_time} Minutes</p>
+                <p>{prove.running_time} Minutes</p>
               </div>
 
 
               <div className="Movie-Right__Button" >
-                <BtnFavorites className="btn__MovieDetail" dataMovie={movieSelected}/>
+                <BtnFavorites className="btn__MovieDetail" dataMovie={prove}/>
               </div>
               
               <div className="Movie-Right__More-Detail">
                 <p>Original Title:</p>
-                <p>{movieSelected.original_title}</p>
+                <p>{prove.original_title}</p>
               </div>
               <div className="Movie-Right__More-Detail">
                 <p>Director:</p>
-                <p>{movieSelected.director}</p>
+                <p>{prove.director}</p>
               </div>
               <div className="Movie-Right__More-Detail">
                 <p>Producer:</p>
-                <p>{movieSelected.producer}</p>
+                <p>{prove.producer}</p>
               </div>
               <div className="Movie-Right__More-Detail">
                 <p>Release Year:</p>
-                <p>{movieSelected.release_date}</p>
+                <p>{prove.release_date}</p>
               </div>
             </div>
           </div>
